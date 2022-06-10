804d98c086e5e9972aed2fa628217e31ab22d2e0
xummi <xummi@LAPTOP-25BPB3QV>
2022-06-10 21:11:35 +0900

    Add a new line

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -2,6 +2,8 @@
 public class MyClass {
 
 	public static void main(String args[]) {
-		System.out.println("Hello GIt"); 	}
+		System.out.println("Hello GIt"); 
+		System.out.println("Hello GIt 222"); 
+	}
 
 }
