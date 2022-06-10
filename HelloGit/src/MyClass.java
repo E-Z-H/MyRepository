8e182055dd964f5bcda9fc0beec6a0cbc2f39f9e
xummi <xummi@LAPTOP-25BPB3QV>
2022-06-10 22:47:38 +0900

    First commit in dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -4,6 +4,10 @@ public class MyClass {
 	public static void main(String args[]) {
 		System.out.println("Hello GIt");  
 		System.out.println("Hello GIt 222"); 
+		
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
+		
 	}
 
 }
