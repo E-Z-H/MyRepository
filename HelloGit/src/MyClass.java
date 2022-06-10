46b335dc6892c53c9daac8a1f2183ee505c3893b
xummi <xummi@LAPTOP-25BPB3QV>
2022-06-10 22:24:16 +0900

    test

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -2,7 +2,7 @@
 public class MyClass {
 
 	public static void main(String args[]) {
-		System.out.println("Hello GIt"); 
+		System.out.println("Hello GIt");  
 		System.out.println("Hello GIt 222"); 
 	}
 
