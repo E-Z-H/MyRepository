29dd46c90b3552ccdc5462cbed0c8e66e3389835
xummi <xummi@LAPTOP-25BPB3QV>
2022-06-10 22:52:25 +0900

    Change in master brach

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 	public static void main(String args[]) {
 		System.out.println("Hello GIt");  
 		System.out.println("Hello GIt 222"); 
+		
+		System.out.println("Change in master branch"); 
 	}
 
 }
